c51d49141036a3e2388a8a419b6618c581a24a3c
Tom k Mangattu <51293980+tomkmangattu@users.noreply.github.com>
2023-04-17 21:43:19 +0530

    Update raspberrypi.py

--- raspberrypi.py
+++ raspberrypi.py
@@ -8,12 +8,16 @@ bus = SMBus(1) # indicates /dev/ic2-1
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+bus.write_byte(addr, 0x1) #starting
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
-        bus.write_byte(addr, 0x1)
-        bus.write_byte(addr, 0x2)
-        bus.write_byte(addr, 0x3)
-        bus.write_byte(addr, 0x4)
+ 
+        myArray = [10, 20, 30, 40]
+        byteArray = bytearray(myArray)
+        for b in byteArray:
+           print(b)
+           bus.write_byte(addr, b)
+        
         time.sleep(1)
